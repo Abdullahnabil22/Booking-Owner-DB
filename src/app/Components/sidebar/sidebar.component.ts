1b7f04a6364bb3fd0cd5a9359757e4b657aa72e5
Abdullahnabil22 <abdullahn.work@gmail.com>
2024-09-28 21:37:21 +0300

    layout update

---
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+    CommonModule,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { AddPropertyComponent } from './Components/add-property/add-property.component';
+import { EditPropertyComponent } from './Components/edit-property/edit-property.component';
+import { ReviewsComponent } from './Components/reviews/reviews.component';
+import { MessagesComponent } from './Components/messages/messages.component';
+import { LoginComponent } from './Components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+  { path: 'edit-property', component: EditPropertyComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'login', component: LoginComponent },
+];
