cd6eb5d747f043c13d4a7b1fa8f79c2e5e8fca1f
Abdullahnabil22 <abdullahn.work@gmail.com>
2024-09-29 02:28:02 +0300

    add hosts changes

--- src/app/Components/add-property/add-property.component.ts
+++ src/app/Components/add-property/add-property.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-add-property',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './add-property.component.html',
-  styleUrl: './add-property.component.css'
+  styleUrl: './add-property.component.css',
 })
-export class AddPropertyComponent {
-
-}
+export class AddPropertyComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { EditPropertyComponent } from './Components/edit-property/edit-property.
 import { ReviewsComponent } from './Components/reviews/reviews.component';
 import { MessagesComponent } from './Components/messages/messages.component';
 import { LoginComponent } from './Components/login/login.component';
+import { AddHotelComponent } from './Components/add-hotel/add-hotel.component';
+import { AddApartmentComponent } from './Components/add-apartment/add-apartment.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'add-property', component: AddPropertyComponent },
+  { path: 'add-property/hotel', component: AddHotelComponent },
+  { path: 'add-property/apartment', component: AddApartmentComponent },
   { path: 'edit-property', component: EditPropertyComponent },
   { path: 'reviews', component: ReviewsComponent },
   { path: 'messages', component: MessagesComponent },
