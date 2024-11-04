fd8d7ea791322573e9f26cdcca876308cc2fd34b
Abdullahnabil22 <abdullahn.work@gmail.com>
2024-11-04 02:28:47 +0200

    login updates

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,70 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { LoginService } from '../../Services/login/login.service';
-// import { FormsModule } from '@angular/forms';
-// import { RouterModule, Router } from '@angular/router';
-// import { HttpClientModule } from '@angular/common/http';
-// import { CommonModule } from '@angular/common';
-// import { JWTService } from '../../Services/JWT/jwt.service';
-
-// /////////////////////////
-// @Component({
-//   selector: 'app-login',
-//   standalone: true,
-//   imports: [FormsModule, RouterModule, HttpClientModule, CommonModule],
-//   providers: [LoginService],
-//   templateUrl: './login.component.html',
-//   styleUrl: './login.component.css',
-// })
-// export class LoginComponent implements OnInit {
-//   email: string = '';
-//   password: string = '';
-//   errorMessage: string = '';
-//   isUserLoggedIn: boolean = false;
-//   constructor(
-//     private loginService: LoginService,
-//     private router: Router,
-//     private jwtService: JWTService
-//   ) {}
-
-//   ngOnInit(): void {
-//     this.isUserLoggedIn = this.loginService.isUserLoggedIn;
-//   }
-
-//   login(): void {
-//     this.loginService.login(this.email, this.password).subscribe(
-//       (response) => {
-//         if (response && response.token) {
-//           const decodedToken = this.jwtService.decodeToken(response.token);
-//           if (decodedToken.role === 'owner') {
-//             localStorage.setItem('token', response.token);
-//             this.router.navigate(['/dashboard']);
-//             this.isUserLoggedIn = true;
-//           } else {
-//             this.errorMessage = 'Access denied. You are not authorized.';
-//             console.log('Invalid response structure:', response);
-//           }
-//         }
-//       },
-//       (error) => {
-//         this.errorMessage = 'Access denied.';
-//       }
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../Services/login/login.service';
 import { FormsModule } from '@angular/forms';
@@ -79,13 +12,14 @@ import { JWTService } from '../../Services/JWT/jwt.service';
   imports: [FormsModule, RouterModule, HttpClientModule, CommonModule],
   providers: [LoginService],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'], 
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
   isUserLoggedIn: boolean = false;
+  showPassword = false;
 
   constructor(
     private loginService: LoginService,
@@ -97,12 +31,12 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.isUserLoggedIn = this.loginService.isUserLoggedIn;
 
-    this.activatedRoute.queryParams.subscribe(params => {
+    this.activatedRoute.queryParams.subscribe((params) => {
       const token = params['token'];
-      console.log("Token from URL:", token);
+      console.log('Token from URL:', token);
       if (token) {
         localStorage.setItem('token', token);
-      
+
         this.loginService.loginWithToken(token).subscribe(
           (response) => {
             if (response && response.user) {
@@ -119,10 +53,7 @@ export class LoginComponent implements OnInit {
           },
           (error) => {
             this.errorMessage = 'Access denied.';
-            console.error("Error during login with token:", error); 
-
-
-            
+            console.error('Error during login with token:', error);
           }
         );
       }
@@ -133,21 +64,31 @@ export class LoginComponent implements OnInit {
     this.loginService.login(this.email, this.password).subscribe(
       (response) => {
         if (response && response.token) {
-          const decodedToken = this.jwtService.decodeToken(response.token);
-          if (decodedToken.role === 'owner') {
-            localStorage.setItem('token', response.token);
-            this.router.navigate(['/dashboard']);
-            this.isUserLoggedIn = true;
-            this.loginService.userLog.next(true);
-          } else {
-            this.errorMessage = 'Access denied. You are not authorized.';
-            console.log('Invalid response structure:', response);
+          try {
+            const decodedToken = this.jwtService.decodeToken(response.token);
+            if (decodedToken.role === 'owner') {
+              localStorage.setItem('token', response.token);
+              this.router.navigate(['/dashboard']);
+              this.isUserLoggedIn = true;
+              this.loginService.userLog.next(true);
+            } else {
+              this.errorMessage = 'Access denied. You are not authorized.';
+              console.log('Invalid response structure:', response);
+            }
+          } catch (error) {
+            this.errorMessage = 'Invalid authentication please try again.';
+            console.error('Token decode error:', error);
           }
         }
       },
       (error) => {
         this.errorMessage = 'Access denied.';
+        console.error('Login error:', error);
       }
     );
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
 }
